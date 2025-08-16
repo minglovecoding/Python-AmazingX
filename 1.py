239ca03bea7c06e48752ec2beefc7da68d945ab7
minglovecoding <937399140@qq.com>
2025-08-16 15:48:22 +0800

    update

--- 1.py
+++ 1.py
@@ -1,13 +1,8 @@
-def cnt_diff(N):
-    digits=len(str(N))
-    ans=0
-    for digit in range(1,digits+1):
-        upper=min(N,int('5'+'0'*(digit-1))-1)
-        low=int('4'*digit)
-        ans+=max(upper-low,0)
-    return ans
-
-T=int(input())
-for _ in range(T):
-    N=int(input())
-    print(cnt_diff(N))
+i = 2
+while(i < 100):
+   j = 2
+   while(j <= (i/j)):
+      if not(i%j): break
+      j = j + 1
+   if (j > i/j) : print (i, " 是素数")
+   i = i + 1
