df6328de040f917d6b4586ebad7e9179d854a50e
minglovecoding <937399140@qq.com>
2026-02-02 15:08:21 +0800

    update

--- code/g2.cpp
+++ code/g2.cpp
@@ -1,87 +1,73 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-using ll = long long;
-
-struct BIT {
-    int n;
-    vector<int> tr;
-    BIT(int n=0){ init(n); }
-    void init(int n_) { n = n_; tr.assign(n + 1, 0); }
-    void add(int x, int v) {
-        for (; x <= n; x += x & -x) tr[x] += v;
-    }
-    ll sum(int x) const {
-        ll r = 0;
-        for (; x > 0; x -= x & -x) r += tr[x];
-        return r;
-    }
+struct Edge {
+    int to;
+    char c;
 };
 
 int main() {
     ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
+    cin.tie(NULL);
     int T;
     cin >> T;
-    while (T--) {
-        int n;
-        cin >> n;
-        vector<ll> a(n + 1);
-        vector<ll> b;
-        b.reserve(n);
-        for (int i = 1; i <= n; i++) {
-            cin >> a[i];
-            b.push_back(a[i]);
+    for(int t = 0; t < T; t++) {
+        int N, M;
+        cin >> N >> M;
+        vector<vector<Edge>> adj(N + 1);
+        for(int i = 0; i < M; i++) {
+            int u, v;
+            char c;
+            cin >> u >> v >> c;
+            adj[u].push_back({v, c});
+            adj[v].push_back({u, c});
         }
-        sort(b.begin(), b.end());
-        b.erase(unique(b.begin(), b.end()), b.end());
-        int m = (int)b.size();
-
-        vector<int> rk(n + 1);
-        for (int i = 1; i <= n; i++) {
-            rk[i] = (int)(lower_bound(b.begin(), b.end(), a[i]) - b.begin()) + 1;
-        }
-
-        vector<ll> s(n + 1), g(n + 1);
-        BIT bit(m);
-
-        for (int i = 1; i <= n; i++) {
-            int r = rk[i];
-            s[i] = bit.sum(r - 1);
-            bit.add(r, 1);
-        }
-
-        bit.init(m);
-        for (int i = n; i >= 1; i--) {
-            int r = rk[i];
-            g[i] = bit.sum(r - 1);
-            bit.add(r, 1);
-        }
-
-        vector<vector<int>> pos(m + 1);
-        for (int i = 1; i <= n; i++) pos[rk[i]].push_back(i);
-
-        ll ans = 0;
-        for (int r = 1; r <= m; r++) {
-            auto &v = pos[r];
-            int k = (int)v.size();
-            ll totg = 0;
-            for (int i = 0; i < k; i++) totg += g[v[i]];
-
-            ll preS = 0, preG = 0;
-            ll best = (ll)4e18;
-
-            for (int t = 0; t <= k; t++) {
-                best = min(best, preS + (totg - preG));
-                if (t == k) break;
-                preS += s[v[t]];
-                preG += g[v[t]];
+        vector<int> dist(N + 1, -1);
+        vector<char> visited(N + 1, 0);
+        dist[1] = 0;
+        visited[1] = 1;
+        queue<int> q;
+        q.push(1);
+        int d = 0;
+        while(!q.empty()) {
+            int level_size = q.size();
+            vector<int> current_level;
+            for(int i = 0; i < level_size; i++) {
+                int u = q.front(); q.pop();
+                current_level.push_back(u);
             }
-            ans += best;
+            char c_min = 'z' + 1;
+            for(int u : current_level) {
+                for(const auto& e : adj[u]) {
+                    if(e.c < c_min) {
+                        c_min = e.c;
+                    }
+                }
+            }
+            vector<int> next_level;
+            for(int u : current_level) {
+                for(const auto& e : adj[u]) {
+                    if(e.c == c_min) {
+                        int v = e.to;
+                        if(!visited[v]) {
+                            visited[v] = 1;
+                            dist[v] = d + 1;
+                            next_level.push_back(v);
+                        }
+                    }
+                }
+            }
+            for(int v : next_level) {
+                q.push(v);
+            }
+            d++;
         }
-
-        cout << ans << "\n";
+        for(int i = 1; i <= N; i++) {
+            cout << dist[i];
+            if(i < N) cout << " ";
+            else cout << endl;
+        }
     }
     return 0;
 }
--- code/g3.cpp
+++ code/g3.cpp
@@ -1,119 +1,202 @@
 #include <bits/stdc++.h>
 using namespace std;
-static const long long MOD = 1000000007LL;
 
-struct SegTree {
-    int n;
-    vector<long long> sum, lazy; 
+const int INF = 1e9;
 
-    SegTree(int n_) : n(n_) {
-        sum.assign(4 * n + 5, 0);
-        lazy.assign(4 * n + 5, 1);
+int n, num_marked;
+
+void solve() {
+    cin >> n >> num_marked;
+
+    vector<int> next_node(n + 1);
+    vector<int> in_degree(n + 1);
+    vector<vector<int>> prev_nodes(n + 1);
+
+    for (int i = 1; i <= n; i++) {
+        cin >> next_node[i];
+        in_degree[next_node[i]]++;
+        prev_nodes[next_node[i]].push_back(i);
     }
 
-    inline long long modMul(long long a, long long b) {
-        return (a * b) % MOD;
+    vector<int> dist_from_marked(n + 1, INF);
+    vector<bool> is_marked(n + 1);
+    queue<int> q;
+
+    for (int i = 0; i < num_marked; i++) {
+        int x; cin >> x;
+        is_marked[x] = true;
+        dist_from_marked[x] = 0;
+        q.push(x);
     }
 
-    void push(int node) {
-        if (lazy[node] == 1) return;
-        long long mul = lazy[node];
-        int lc = node * 2, rc = node * 2 + 1;
-
-        sum[lc] = modMul(sum[lc], mul);
-        lazy[lc] = modMul(lazy[lc], mul);
-
-        sum[rc] = modMul(sum[rc], mul);
-        lazy[rc] = modMul(lazy[rc], mul);
-
-        lazy[node] = 1;
-    }
-
-    void rangeMul(int node, int l, int r, int ql, int qr, long long mul) {
-        if (ql > r || qr < l) return;
-        if (ql <= l && r <= qr) {
-            sum[node] = modMul(sum[node], mul);
-            lazy[node] = modMul(lazy[node], mul);
-            return;
+    while (!q.empty()) {
+        int u = q.front(); q.pop();
+        int v = next_node[u];
+        if (dist_from_marked[v] == INF) {
+            dist_from_marked[v] = dist_from_marked[u] + 1;
+            q.push(v);
         }
-        push(node);
-        int mid = (l + r) >> 1;
-        rangeMul(node * 2, l, mid, ql, qr, mul);
-        rangeMul(node * 2 + 1, mid + 1, r, ql, qr, mul);
-        sum[node] = (sum[node * 2] + sum[node * 2 + 1]) % MOD;
     }
 
-    long long rangeSum(int node, int l, int r, int ql, int qr) {
-        if (ql > r || qr < l) return 0;
-        if (ql <= l && r <= qr) return sum[node];
-        push(node);
-        int mid = (l + r) >> 1;
-        return (rangeSum(node * 2, l, mid, ql, qr) +
-                rangeSum(node * 2 + 1, mid + 1, r, ql, qr)) % MOD;
+    vector<bool> on_cycle(n + 1, true);
+    for (int i = 1; i <= n; i++) {
+        if (!in_degree[i]) q.push(i);
     }
 
-    void pointAdd(int node, int l, int r, int idx, long long val) {
-        if (l == r) {
-            sum[node] = (sum[node] + val) % MOD;
-            return;
+    while (!q.empty()) {
+        int u = q.front(); q.pop();
+        on_cycle[u] = false;
+        int v = next_node[u];
+        if (--in_degree[v] == 0) q.push(v);
+    }
+
+    vector<int> cycle_id(n + 1, -1);
+    vector<int> cycle_pos(n + 1);
+    vector<int> cycle_root(n + 1);
+    vector<int> cycle_len;
+    int current_cycle_count = 0;
+
+    for (int i = 1; i <= n; i++) {
+        if (on_cycle[i] && cycle_id[i] == -1) {
+            int u = i, p = 0;
+            while (cycle_id[u] == -1) {
+                cycle_id[u] = current_cycle_count;
+                cycle_pos[u] = p++;
+                cycle_root[u] = u;
+                u = next_node[u];
+            }
+            cycle_len.push_back(p);
+            current_cycle_count++;
         }
-        push(node);
-        int mid = (l + r) >> 1;
-        if (idx <= mid) pointAdd(node * 2, l, mid, idx, val);
-        else pointAdd(node * 2 + 1, mid + 1, r, idx, val);
-        sum[node] = (sum[node * 2] + sum[node * 2 + 1]) % MOD;
     }
 
-    void rangeMul(int l, int r, long long mul) {
-        if (l > r) return;
-        rangeMul(1, 1, n, l, r, mul);
-    }
-    long long rangeSum(int l, int r) {
-        if (l > r) return 0;
-        return rangeSum(1, 1, n, l, r);
-    }
-    void pointAdd(int idx, long long val) {
-        pointAdd(1, 1, n, idx, val);
-    }
-};
+    vector<int> dist_to_cycle(n + 1);
+    vector<vector<int>> group_nodes(n + 1);
 
-int main() {
+    for (int i = 1; i <= n; i++) {
+        if (on_cycle[i]) q.push(i);
+    }
+
+    while (!q.empty()) {
+        int u = q.front(); q.pop();
+        group_nodes[cycle_root[u]].push_back(u);
+        
+        for (int v : prev_nodes[u]) {
+            if (!on_cycle[v]) {
+                cycle_id[v] = cycle_id[u];
+                cycle_root[v] = cycle_root[u];
+                dist_to_cycle[v] = dist_to_cycle[u] + 1;
+                q.push(v);
+            }
+        }
+    }
+
+    vector<vector<bool>> banned_mod(current_cycle_count);
+    vector<vector<int>> tree_obstacles(n + 1);
+
+    for (int i = 1; i <= n; i++) {
+        if (dist_from_marked[i] == 0 && is_marked[i]) {
+            int cid = cycle_id[i];
+            int l = cycle_len[cid];
+
+            long long val = (long long)dist_to_cycle[i] - cycle_pos[cycle_root[i]];
+            int m = (val % l + l) % l;
+
+            if (banned_mod[cid].empty()) banned_mod[cid].assign(l, false);
+            banned_mod[cid][m] = true;
+
+            if (!on_cycle[i]) tree_obstacles[cycle_root[i]].push_back(dist_to_cycle[i]);
+        }
+    }
+
+    vector<bool> cycle_blocked(current_cycle_count);
+    vector<vector<int>> prev_valid_mod(current_cycle_count);
+
+    for (int i = 0; i < current_cycle_count; i++) {
+        if (banned_mod[i].empty()) continue;
+        cycle_blocked[i] = true;
+        for (int j = 0; j < cycle_len[i]; j++) {
+            if (!banned_mod[i][j]){
+                cycle_blocked[i] = false;
+                break;
+            }
+        }
+        if (!cycle_blocked[i]) {
+            int l = cycle_len[i];
+            prev_valid_mod[i].resize(2 * l);
+            int last = -1;
+            for (int j = 0; j < 2 * l; j++) {
+                int m = j % l;
+                if (!banned_mod[i][m]) last = j;
+                prev_valid_mod[i][j] = last;
+            }
+        }
+    }
+
+    vector<int> ans(n + 1);
+    vector<int> valid_prefix_pos(n + 1);
+
+    for (int r = 1; r <= n; r ++){
+        if (!on_cycle[r] || group_nodes[r].empty()) continue;
+
+        int cid = cycle_id[r];
+        int l = cycle_len[cid];
+        int max_depth = -1;
+        
+        for (int x : tree_obstacles[r]) max_depth = max(max_depth, x);
+
+        if (max_depth != -1) {
+            int last_ok = -1;
+            for (int x = 0; x <= max_depth; x++) {
+                long long val = (long long)x - cycle_pos[r];
+                int m = (val % l + l) % l;
+                bool ok = true;
+                if (!banned_mod[cid].empty() && banned_mod[cid][m]) ok = false;
+                valid_prefix_pos[x] = ok ? x : -1;
+            }
+            for (int x : tree_obstacles[r]) valid_prefix_pos[x] = -1;
+            
+            for (int x = 0; x <= max_depth; x++) {
+                if (valid_prefix_pos[x] != -1) last_ok = valid_prefix_pos[x];
+                valid_prefix_pos[x] = last_ok;
+            }
+        }
+
+        for (int u : group_nodes[r]) {
+            if (is_marked[u]) { ans[u] = -1; continue; }
+            if (dist_from_marked[u] == INF) { ans[u] = -2; continue; }
+            if (cycle_blocked[cid]) { ans[u] = -1; continue; }
+
+            int limit_step = dist_to_cycle[u] + dist_from_marked[u] - 1;
+            int best_reach = -1;
+
+            if (limit_step > max_depth) {
+                long long val = (long long)limit_step - cycle_pos[r];
+                int m = (val % l + l) % l;
+                
+                int g = prev_valid_mod[cid][m + l];
+                int diff = (m + l) - g;
+                int cand = limit_step - diff;
+                
+                if (cand > max_depth) best_reach = cand;
+                else if (max_depth != -1) best_reach = valid_prefix_pos[max_depth];
+            } else {
+                if (max_depth != -1 && limit_step >= 0) best_reach = valid_prefix_pos[limit_step];
+            }
+
+            if (best_reach < dist_to_cycle[u]) ans[u] = -1;
+            else ans[u] = best_reach - dist_to_cycle[u];
+        }
+    }
+
+    for (int i = 1; i <= n; i++) cout << ans[i] << "\n";
+}
+
+signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int N;
-    long long D;
-    cin >> N >> D;
+    solve();
 
-    vector<long long> p(N + 1);
-    vector<int> o(N + 1);
-    for (int i = 1; i <= N; i++) {
-        cin >> p[i] >> o[i];
-    }
-
-    SegTree st(N);
-
-    long long total = 1; 
-    int ptr = 1;         
-
-    for (int i = 1; i <= N; i++) {
-        if (o[i] == 1) {
-            st.pointAdd(i, total);
-            total = (2 * total) % MOD;
-        } else {
-            long long need = p[i] - D;
-            while (ptr < i && p[ptr] < need) ptr++;
-            int L = ptr;
-
-            long long eligible_sum = st.rangeSum(L, i - 1);
-            st.rangeMul(L, i - 1, 2);
-            total += eligible_sum;
-            if (total >= MOD) total -= MOD;
-        }
-    }
-
-    long long ans = total - 1; 
-    if (ans < 0) ans += MOD;
-    cout << ans << "\n";
     return 0;
 }
