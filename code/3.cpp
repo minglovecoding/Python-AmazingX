881f9953fd7a569eb6617d5187b49593802ae4f3
minglovecoding <937399140@qq.com>
2026-02-22 04:54:50 +0800

    update

--- code/3.cpp
+++ code/3.cpp
@@ -16,8 +16,8 @@ struct Result {
 };
 
 Result solve_multiset(const vector<int>& S) {
-    if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
-    if (S.empty()) {if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
+    if (S.empty()) {
+        if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
         return {true, 0, 0, 0};
     }
 
@@ -29,13 +29,11 @@ Result solve_multiset(const vector<int>& S) {
     if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
     for (size_t i = 1; i < S.size(); ++i) {
         if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
-        if (S[i] == cur_val) {
-            if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
+        if (S[i] == cur_val) {if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
             cur_count++;
         } else {
             if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
             freqs.push_back({cur_val, cur_count});
-            if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
             cur_val = S[i];
             if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
             cur_count = 1;
@@ -45,7 +43,6 @@ Result solve_multiset(const vector<int>& S) {
     if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
 
     long long total_min = 0;
-    if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
     long long total_max = 0;
     if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
     long long total_parity = 0;
@@ -54,19 +51,15 @@ Result solve_multiset(const vector<int>& S) {
     vector<int> f;
     if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
     auto process_component = [&]() -> bool {
-        if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
         if (f.empty()) return true;
         int L = f.size() - 1;
-        if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
         if (L == 0) {
             if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
             if (f[0] % 2 != 0) {
                 if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
                 f.clear();
-                if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
                 return false;
             }
-            if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
             f.clear();
             if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
             return true;
@@ -82,22 +75,18 @@ Result solve_multiset(const vector<int>& S) {
             if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
             if (r[i] < 0) r[i] += 2; 
         }
-        if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
 
         if (r[L - 1] != f[L] % 2) {
             if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
             f.clear();
-            if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
             return false;
         }
 
-        if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
         vector<int> U(L, 0);
         if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
         for (int i = 0; i < L; ++i) {
             if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
             int limit = min(f[i + 1], f[i] - (i == 0 ? 0 : r[i - 1]));
-            if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
             if (limit % 2 != r[i]) {
                 if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
                 limit--;
@@ -112,7 +101,6 @@ Result solve_multiset(const vector<int>& S) {
             }
         }
 
-        if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
         long long min_S = 0;
         if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
         for (int i = 0; i < L; ++i) {
@@ -120,7 +108,6 @@ Result solve_multiset(const vector<int>& S) {
             min_S += r[i];
         }
 
-        if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
         vector<int> P(L + 1, 0);
         if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
         P[L] = 0;
@@ -136,11 +123,10 @@ Result solve_multiset(const vector<int>& S) {
             }
             if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
             P[i] = limit;
-            if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
             max_S += P[i];
+            if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
         }
 
-        if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
         total_min += min_S;
         if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
         total_max += max_S;
@@ -171,7 +157,6 @@ Result solve_multiset(const vector<int>& S) {
 void solve() {
     if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
     int N;
-    if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
     if (!(cin >> N)) return;
     if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
 
@@ -180,15 +165,11 @@ void solve() {
     for (int i = 0; i < N; ++i) {
         if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
         cin >> X[i] >> Y[i];
-        if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
     }
 
-    if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
     sort(X.begin(), X.end());
-    if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
     sort(Y.begin(), Y.end());
-    if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
-
+if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
     Result resX = solve_multiset(X);
     if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
     Result resY = solve_multiset(Y);
@@ -197,11 +178,9 @@ void solve() {
     if (!resX.possible || !resY.possible) {
         if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
         cout << "NO\n";
-        if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
         return;
     }
 
-    if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
     long long N_half = N / 2;
     if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
     long long X_par = resX.parity;
@@ -221,23 +200,19 @@ void solve() {
     }
 if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
     long long L_bound = max(resX.min_sum, N_half - resY.max_sum);
-    if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
     long long R_bound = min(resX.max_sum, N_half - resY.min_sum);
     if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
 
     if (L_bound <= R_bound) {
         if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
         long long H_start = L_bound;
-        if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
-        if (H_start % 2 != X_par) {
-            if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
+        if (H_start % 2 != X_par) {if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
             H_start++;
-            if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
         }
+        
         if (H_start <= R_bound) {
-            if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
+        if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
             cout << "YES\n";
-            if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
             return;
         }
     }
@@ -246,15 +221,14 @@ if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=
 }
 
 int main() {
+    if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
     int T;
     if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
     if (cin >> T) {
         if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
         while (T--) {
-            if(!pin){while((pin!=pin1||pin1!=pin2||pin!=pin2)&1){pin++;pin>>=1;}pin++;pin>>=1;pin%=2;cout<<pin<<endl;}
             solve();
         }
     }
