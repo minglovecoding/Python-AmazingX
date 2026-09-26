43f523cc95f4fbd47bf3082254dc46471d7ccf84
minglovecoding <937399140@qq.com>
2026-09-26 13:52:25 +0800

    update

--- code/1.cpp
+++ code/1.cpp
@@ -1,11 +1,61 @@
-//从字串p入手
-//f[i][j][p]=f[i-1][j][p]+g[i][j][p]
-//g[i][j][p]=g[i−1][j−1][p]+f[i−1][j−1][p−1]
-//f[i][0][0]=1
-//f[n][m][k]
 #include<bits/stdc++.h>
 using namespace std;
+int dx[4]={1,0,-1,0};
+int dy[4]={0,1,0,-1};
+set<pair<int,int>>cows;
+queue<pair<int,int>>q;
+int cnt=0;
+bool hasCow(int x,int y){
+    return cows.count({x,y})>0;
+}
+
+void addCow(int x,int y){
+    if(hasCow(x,y)) return;
+    cows.insert({x,y});
+    cnt++;
+    q.push({x,y});
+}
+
+void checkStatus(int x,int y){
+    if(!hasCow(x,y)) return;
+    int num=0;
+    int emptyX=0;
+    int emptyY=0;
+    for(int i=0;i<4;i++){
+        int nx=x+dx[i];
+        int ny=y+dy[i];
+        if(hasCow(nx,ny)) num++;
+        else{
+            emptyX=nx;
+            emptyY=ny;
+        }
+    }
+    if(num==3) addCow(emptyX,emptyY);
+}
+
 int main(){
-    cout<<"hello"<<endl;
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N;
+    cin>>N;
+
+    for(int i=1;i<=N;i++){
+        int x,y;
+        cin>>x>>y;
+        addCow(x,y);
+        while(!q.empty()){
+            auto [x,y]=q.front();
+            q.pop();
+
+            checkStatus(x,y);
+            for(int i=0;i<4;i++){
+                int nx=x+dx[i];
+                int ny=y+dy[i];
+                checkStatus(nx,ny);
+            }
+        }
+        cout<<cnt-i<<"\n";
+    }
     return 0;
 }
--- code/2.cpp
+++ code/2.cpp
@@ -1,3 +1,41 @@
-//1.求所有的空白点到附近墙壁的最短距离
-// (T/D)down< 中心点到墙壁的距离
-//
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int N,K;
+    cin>>N>>K;
+
+    vector<ll>layers;
+    layers.push_back(0);
+    for(int i=0;i<N;i++){
+        ll year;
+        cin>>year;
+        ll layer=(year+11)/12;
+        layers.push_back(layer);
+    }
+    
+    //sort and unique
+    sort(layers.begin(),layers.end());
+    layers.erase(unique(layers.begin(),layers.end()),layers.end());
+
+    ll answer=layers.back()*12;  //biggest gap time
+    vector<ll>gaps;
+
+    for(int i=1;i<layers.size();i++){
+        ll empty=layers[i]-layers[i-1]-1; //must -1
+        gaps.push_back(empty*12);
+    }
+    
+    sort(gaps.rbegin(),gaps.rend());
+    int skip=min(K-1,(int)gaps.size());
+    
+    for( int i=0;i<skip;i++){
+        answer-=gaps[i];
+    }
+    
+    cout<<answer<<"\n";
+    return 0;
+}
